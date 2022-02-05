6841087adfcf67354b968569b98ad83326fcd5e1
Neil <neilmonastiri@gmail.com>
2022-02-05 13:49:33 +0100

    3ich Healthy App commit

--- app/src/androidTest/java/com/example/a3ichhealthy/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/a3ichhealthy/ExampleInstrumentedTest.java
@@ -1,21 +1,21 @@
-package com.example.a3ichhealthy;
+/* package com.example.a3ichhealthy;
+
+import static org.junit.Assert.*;
 
 import android.content.Context;
 
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-@RunWith(AndroidJUnit4.class)
+/* @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() {
@@ -24,3 +24,4 @@ public class ExampleInstrumentedTest {
         assertEquals("com.example.a3ichhealthy", appContext.getPackageName());
     }
 }
+*/
